be11677d4969cb2c4d3b9a6e4b18e109e9b641df
Scott Gregory <diggndeeper.com@gmail.com>
2023-03-30 07:13:06 -0400

    rough blocking

---
+++ wp-dapp.php
@@ -0,0 +1,13 @@
+ 
+/*
+ * Plugin Name:       Hive interface.
+ * Plugin URI:        https://diggndeeper.com/wp-dapp/
+ * Description:       Update, publish and more.
+ * Version:           0.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            DiggnDeeper
+ * Author URI:        https://diggndeeper.com
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
